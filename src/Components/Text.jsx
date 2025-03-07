80da3705d19367fe80ed4b069ff0c2edbe81f162
Ashutosh <aashu1902ap@gmail.com>
2025-03-07 16:46:35 +0530

    Add text

---
+++ src/Components/Text.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Draggable from 'react-draggable'
+
+
+function Text() {
+  return (
+    
+    <Draggable>
+        <div>
+         <h1>I am a text</h1>
+      </div>
+    </Draggable>
+    
+  )
+}
+
+export default Text
--- src/Pages/Edit.jsx
+++ src/Pages/Edit.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSearchParams } from 'react-router'
+import Text from '../Components/Text';
 
 function Edit() {
 
     const [params] = useSearchParams();
+    const [count, setCount] = useState(0);
     console.log(params.get("url")) 
+
+    const addText = () =>{
+        setCount(count + 1);
+    }
 
   return (
     <div>
+        <div>
       <img src={params.get("url")} width="500px" alt="" />
+      {Array(count).fill(0).map((e) => <Text/>)}
+        </div>
+        <button onClick={addText}>Add Text</button>
     </div>
   )
 }
