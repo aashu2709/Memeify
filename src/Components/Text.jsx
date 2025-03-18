4a8eb5f83c0bf65cec75d830ba1f3057b2727b0f
Ashutosh <aashu1902ap@gmail.com>
2025-03-18 20:06:06 +0530

    new  changes

--- src/Components/Text.jsx
+++ src/Components/Text.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import Draggable from 'react-draggable'
+import React, { useState } from 'react'
+import {DraggableCore, Draggable} from 'react-draggable' 
 
 
 function Text() {
+
+  const [editMode, setEditMode] = useState(false);
+  const [val, setVal] = useState("Double Click to Edit");
+
   return (
     
-    <Draggable>
-        <div>
-         <h1>I am a text</h1>
-      </div>
-    </Draggable>
-    
+      <Draggable>
+        {editMode ? (
+          <input 
+          onDoubleClick={(e) => setEditMode(false)}
+          value={val}
+          onChange={(e) => setVal(e.target.value)} 
+          />
+
+        ) : (
+          <h1 onDoubleClick={(e) => setEditMode(true)}>{val}</h1>
+        )}
+         
+      
+      </Draggable>
+  
   )
 }
 
--- src/Pages/Edit.jsx
+++ src/Pages/Edit.jsx
@@ -1,26 +1,32 @@
-import React, { useState } from 'react'
-import { useSearchParams } from 'react-router'
+import React, { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Text from '../Components/Text';
+import {exportComponentAsJPEG} from 'react-component-export-image'
 
 function Edit() {
+  const [params] = useSearchParams();
+  const [count, setCount] = useState(0);
 
-    const [params] = useSearchParams();
-    const [count, setCount] = useState(0);
-    console.log(params.get("url")) 
+  const memeRef = createRef();
 
-    const addText = () =>{
-        setCount(count + 1);
-    }
+  console.log(params.get("url"));
+
+  const addText = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div>
-        <div>
-      <img src={params.get("url")} width="500px" alt="" />
-      {Array(count).fill(0).map((e) => <Text/>)}
-        </div>
-        <button onClick={addText}>Add Text</button>
+      <div ref={memeRef} className='meme mt-5 mb-5'>
+        <img src={params.get("url") || ""} width="300px" alt="Uploaded content" />
+        {Array(count).fill(0).map((_, index) => (
+          <Text key={index} />
+        ))}
+      </div>
+      <button onClick={addText}>Add Text</button>
+      <button variant="success" onClick={(e) => {exportComponentAsJPEG(memeRef)}} >Save</button>
     </div>
-  )
+  );
 }
 
-export default Edit
+export default Edit;
